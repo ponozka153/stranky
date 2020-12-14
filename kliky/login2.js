3e9f20da8159c5015082a1cebd37a6a098479f11
ponozka153 <61433633+ponozka153@users.noreply.github.com>
2020-12-14 18:53:25 +0100

    jez

--- kliky/login2.js
+++ kliky/login2.js
@@ -8,3 +8,22 @@ function kliky()
     alert(`Vaše K/D je ${kd}
         Váš počet kliků je ${kliks}`)
 }
+function kliky2()
+{
+    var kill=document.getElementById("kill").value
+    var death=document.getElementById("death").value
+    var pozice=document.getElementById("position").value
+    var kd= kill / death
+    var kliks= kill / death * pozice
+    alert(`Vaše K/D je ${kd}
+        Váš počet kliků je ${kliks}`)  
+}
+function kliky3()
+{
+    var kill=document.getElementById("kill").value
+    var death=document.getElementById("death").value
+    var kd= kill / death
+    var kliks = kd * 2
+    alert(`Vaše K/D je ${kd}
+        Váš počet kliků je ${kliks}`)  
+}
